588ebe33bd0abacd9dfcae51cedc7dbdc17329b3
Мария Константинова <wildcat.3333@yandex.ru>
2023-04-19 22:37:08 +0700

    add ReviewViewSet, CommentViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
-# Create your views here.
+from .permissions import IsAuthorOrReadOnly
+from .serializers import CommentSerializer, ReviewSerializer
+from reviews.models import Review, Title
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    serializer_class = ReviewSerializer
+    pagination_class = LimitOffsetPagination
+
+    def get_title(self):
+        return get_object_or_404(Title, id=self.kwargs.get("title_id"))
+
+    def get_queryset(self):
+        title = self.get_title()
+        return title.reviews
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            title=self.get_title(),
+        )
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    serializer_class = CommentSerializer
+    pagination_class = LimitOffsetPagination
+
+    def get_review(self):
+        title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
+        return get_object_or_404(
+            Review, id=self.kwargs.get("review_id"), title=title)
+
+    def get_queryset(self):
+        review = self.get_review()
+        return review.comments
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            review=self.get_review()
+        )
