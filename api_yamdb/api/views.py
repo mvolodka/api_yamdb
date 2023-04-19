acec43142ff40dd022d64733757592cbe67a00ef
Vladimir Miheikin <basketbolnyy@mail.ru>
2023-04-19 09:52:28 +0300

    created app api

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    ...
+
+
+class GenresViewSet(viewsets.ModelViewSet):
+    ...
+
+
+class CategoriesViewSet(viewsets.ModelViewSet):
+    ...
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    ...
+
+
+class ReviewsViewSet(viewsets.ModelViewSet):
+    ...
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    ...
