b64e69e1011635ee41a23584113d6fa1a8d2b89d
Vladimir Miheikin <basketbolnyy@mail.ru>
2023-04-20 07:48:27 +0300

    created views for Genre, Title, Category

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,25 +1,39 @@
-from rest_framework import viewsets
+from api.filters import TitleFilter
+from rest_framework import filters, viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from reviews.models import Category, Genre, Title
+
+from .serializers import (CategorySerializer, GenreSerializer,
+                          TitleGetSerializer,
+                          TitleWriteSerializer)
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    ...
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    filter_backends = (filters.SearchFilter)
+    search_fields = ("=name",)
 
 
-class GenresViewSet(viewsets.ModelViewSet):
-    ...
-
-
-class CategoriesViewSet(viewsets.ModelViewSet):
-    ...
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    filter_backends = (filters.SearchFilter)
+    search_fields = ("=name",)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    ...
+    queryset = Title.objects.all()
+    pagination_class = PageNumberPagination
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    filterset_class = TitleFilter
 
-
-class ReviewsViewSet(viewsets.ModelViewSet):
-    ...
-
-
-class CommentsViewSet(viewsets.ModelViewSet):
-    ...
+    def get_serializer_class(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return TitleGetSerializer
+        return TitleWriteSerializer
