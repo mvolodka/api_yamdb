52dc0b022cc75c6be1914799b947075d9095b083
Александр Скрынник <cristiano051985@gmail.com>
2023-04-21 22:06:45 +0700

    Настроены роутеры для эндпойнта users/, auth/

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,31 +1,18 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (
-    CommentsViewSet, ReviewsViewSet,
-    CategoriesViewSet, GenresViewSet, TitleViewSet
-)
-from api.views import UserViewSet
+from api.views import CreateUserViewSet, GetTokenViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
-router.register('categories', CategoriesViewSet, basename='categories')
-router.register('genres', GenresViewSet, basename='genres')
-router.register('titles', TitleViewSet, basename='titles')
-router.register(
-    r'^titles/(?P<title_id>\d+)/reviews',
-    ReviewsViewSet,
-    basename='reviews'
-)
-router.register(
-    r'^titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentsViewSet,
-    basename='comments'
-)
+
 
 urlpatterns = [
-    path('v1/users/me/', UserViewSet.as_view(), name='me'),
-    path('v1/auth/signup/', UserViewSet.as_view(), name='signup'),
-    path('v1/auth/token/', UserViewSet.as_view(), name='token'),
+    path('v1/auth/signup/',
+         CreateUserViewSet.as_view({'post': 'create'}),
+         name='signup'),
+    path('v1/auth/token/',
+         GetTokenViewSet.as_view({'post': 'create'}),
+         name='token'),
     path('v1/', include(router.urls)),
 ]
