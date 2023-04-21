61cae6868e1f207ad3dc997133b0c5c87765fd67
Александр Скрынник <cristiano051985@gmail.com>
2023-04-21 22:09:58 +0700

    Регистрация модели User в админке

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from .models import Title, Genre, Category
+from .models import User
 
-admin.site.register(Title)
-admin.site.register(Genre)
-admin.site.register(Category)
+admin.site.register(User)
