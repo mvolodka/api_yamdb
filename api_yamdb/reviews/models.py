c288bcae0d92449a6ea73427df334e79cc6948cf
Александр Скрынник <cristiano051985@gmail.com>
2023-04-21 22:12:09 +0700

    Описана кастомная модель User

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,80 +1,64 @@
-from datetime import datetime
-
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+
+CHOICES = (
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Админ'),
+)
 
 
-class Genre(models.Model):
-    name = models.CharField('Жанр', max_length=100)
-    slug = models.SlugField(max_length=50, unique=True)
-
-    class Meta:
-        ordering = ('slug',)
-
-    def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    name = models.CharField('Категория', max_length=100)
-    slug = models.SlugField(max_length=50, unique=True)
-
-    class Meta:
-        ordering = ('slug',)
-
-    def __str__(self):
-        return self.name
-
-
-class Title(models.Model):
-    name = models.CharField('Название произведения', max_length=100)
-    year = models.PositiveSmallIntegerField(
-        'Год выпуска',
-        validators=[
-            MinValueValidator(
-                0,
-                message='Значение года не может быть меньше 0!'
-            ),
-            MaxValueValidator(
-                int(datetime.now().year),
-                message='Указанный год еще не наступил!'
-            )
-        ],
-        db_index=True
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        verbose_name='Имя пользователя',
+        unique=True,
+        db_index=True,
+        validators=[RegexValidator(
+            regex=r'^[\w.@+-]+$',
+            message='Имя пользователя содержит недопустимый символ'
+        )]
     )
-    description = models.TextField('Описание')
-    genre = models.ManyToManyField(Genre, through='GenreTitle')
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.SET_NULL,
-        null=True,
-        related_name='titles',
-        verbose_name='Категория',
-        help_text='Выберите категорию'
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='email',
+        unique=True
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='имя',
+        blank=True
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='фамилия',
+        blank=True
+    )
+    bio = models.TextField(
+        verbose_name='биография',
+        blank=True
+    )
+    role = models.CharField(
+        max_length=20,
+        verbose_name='роль',
+        choices=CHOICES,
+        default='user'
     )
 
     class Meta:
-        ordering = ('name',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['-id']
 
     def __str__(self):
-        return self.name
+        return self.username
 
+    def is_admin(self):
+        return self.role == 'admin'
 
-class GenreTitle(models.Model):
-    title = models.ForeignKey(
-        Title,
-        on_delete=models.CASCADE,
-        related_name='genretitles',
-    )
-    genre = models.ForeignKey(
-        Genre,
-        on_delete=models.SET_NULL,
-        null=True,
-        related_name='genretitles',
-    )
+    def is_moderator(self):
+        return self.role == 'moderator'
 
-    class Meta:
-        ordering = ('genre',)
-
-    def __str__(self):
-        return f'Произведение: {self.title}, жанр:{self.genre}'
+    def is_user(self):
+        return self.role == 'user'
