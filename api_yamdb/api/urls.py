53b808fedbff0105b1620d44404a9dc9ba39fe33
Мария Константинова <wildcat.3333@yandex.ru>
2023-04-23 18:04:52 +0700

    change priority urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -11,6 +11,7 @@ router_v1 = DefaultRouter()
 router_v1.register('users', UserViewSet, basename='users')
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register('titles', TitleViewSet, basename='titles')
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet,
     basename='review')
@@ -18,15 +19,14 @@ router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comment')
-router_v1.register('titles', TitleViewSet, basename='titles')
 
 
 urlpatterns = [
+    path('v1/', include(router_v1.urls)),
     path('v1/auth/signup/',
          CreateUserViewSet.as_view({'post': 'create'}),
          name='signup'),
     path('v1/auth/token/',
          GetTokenViewSet.as_view({'post': 'create'}),
-         name='token'),
-    path('v1/', include(router_v1.urls)),
+         name='token')
 ]
