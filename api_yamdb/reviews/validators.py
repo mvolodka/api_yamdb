bae5924d47d88937a2756f72c7e250a57f270018
Vladimir Miheikin <basketbolnyy@mail.ru>
2023-04-23 09:23:11 +0300

    updated validators

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,12 +1,18 @@
 import datetime as dt
 
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
 
 def validate_year(year):
     """Year field validation."""
-    current_year = dt.date.today().year
-    if year > current_year or year < 1:
+    if year > dt.date.today().year or year < 1:
         raise ValidationError(
             'Значение года не может быть больше текущего или меньше 1.'
         )
+
+
+slug_validator = [RegexValidator(
+    regex=r'^[-a-zA-Z0-9_]+$',
+    message='Поле "slug" содержит недопустимый символ.'
+)]
