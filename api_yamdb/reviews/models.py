5310636351cc78e86810e675f33e6d164741ae83
Мария Константинова <wildcat.3333@yandex.ru>
2023-04-24 20:40:35 +0700

    changes pep8 import

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
+from django.core.validators import (
+    RegexValidator, MaxValueValidator, MinValueValidator)
 from django.db import models
 from django.db import models
 from django.utils.translation import gettext_lazy as _
