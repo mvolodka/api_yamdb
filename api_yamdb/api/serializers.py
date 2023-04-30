03817e8ff3b23359620b04e588bd66e79ec2fa01
Vladimir Miheikin <vld.mkh@gmail.com>
2023-04-30 23:12:39 +0300

    Changed imports in serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,8 +1,7 @@
 from django.db.models import Avg, Q
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-
-from reviews.models import Category, Genre, Title, Review, Comment, User
+from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
 class CategorySerializer(serializers.ModelSerializer):
